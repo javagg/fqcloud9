c07392266af119d6a1fdc375f356365c54985235
javagg <lu.lee05@gmail.com>
2013-11-16 09:42:01 +0800

    packed can build when pushed

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 var path = require('path');
 var architect = require("architect");
 var spawn = require("child_process").spawn;
+var argv = require('optimist').argv;
 var fs = require("fs");
+var sh = require("execSync");
 
 // TODO: Need better args parser.
 
@@ -21,48 +23,54 @@ if (configName.indexOf("-") === 0) {
 var title_parts = process.argv.slice();
 title_parts.forEach(function(element, index, array) {
     if (element === '--password') {
-        array[index+1] = 'xxxxxxxx';
+        array[index + 1] = 'xxxxxxxx';
     }
 });
 process.title = title_parts.join(' ');
 
-var debug = false;
-var packed = false;
-var packedName = "";
+var debug = argv.d || false;
+var packed = argv.P !== undefined;
+var packedName = (argv.P === true) ? "c9os.min.js" : argv.P;
 var exists = fs.existsSync || path.existsSync;
 
-for (var p = 2; p < process.argv.length; p++) {
-    if (process.argv[p] === "-d") {
-        debug = true;
+console.log("Node location: " + sh.exec('which node 2>&1').stdout + "Please set PATH if you want to use other Node\n")
 
-        // apf debug doesn't exist, copy it
-        var apf_debug = "node_modules/cloud9/plugins-client/lib.apf/www/apf-packaged/apf_debug.js"
-        var apf_release = "node_modules/cloud9/plugins-client/lib.apf/www/apf-packaged/apf_release.js"
-        if (!exists(apf_debug)) {
-            fs.createReadStream(apf_release).pipe(fs.createWriteStream(apf_debug));
-        }
-        boot();
+if (debug) {
+    // apf debug doesn't exist, copy it
+    var apf_debug = "node_modules/cloud9/plugins-client/lib.apf/www/apf-packaged/apf_debug.js"
+    var apf_release = "node_modules/cloud9/plugins-client/lib.apf/www/apf-packaged/apf_release.js"
+    if (!exists(apf_debug)) {
+        fs.createReadStream(apf_release).pipe(fs.createWriteStream(apf_debug));
     }
-
-    else if (process.argv[p] === "-P") {
-        packed = true;
-        if (process.argv[p + 1] && process.argv[p + 1].indexOf("-") < 0) // use this specific packed file
-            packedName = process.argv[++p];
-        else
-            packedName = "c9os.min.js";
-
-        configName = "packed";
-
-        if (!exists("node_modules/cloud9/plugins-client/lib.packed/www/" + packedName) && !exists("node_modules/cloud9/plugins-client/lib.packed/www/" + packedName + ".gz")) {
-            console.error('Packed file not found', 211);
-        }
-        else
-            boot();
-    }
-}
-
-if (debug == false && packed == false)
     boot();
+} else if (packed) {
+    configName = "packed";
+    if (!exists("node_modules/cloud9/plugins-client/lib.packed/www/" + packedName) && !exists("node_modules/cloud9/plugins-client/lib.packed/www/" + packedName + ".gz")) {
+        console.log("Building packed file for first run...Please wait...");
+        console.log("   |\\      _,,,---,,_\n" +
+            "   /,`.-'`'    -.  ;-;;,_\n" +
+            "   |,4-  ) )-,_..;\\ (  `'-'\n" +
+            "   '---''(_/--'  `-'\\_)  Felix Lee");
+
+        var buildPackage = spawn("make", ["package"]);
+
+        buildPackage.stderr.setEncoding("utf8");
+        buildPackage.stderr.on('data', function (data) {
+            console.error(data);
+        });
+        buildPackage.on('exit', function (code) {
+            if (code !== 0) {
+                console.error('build-package process exited with code ' + code);
+                process.exit(code);
+            }
+            boot();
+        });
+    } else {
+        boot()
+    }
+} else {
+    boot();
+}
 
 function boot() {
     var configPath = path.resolve(__dirname, "./configs/", configName);
